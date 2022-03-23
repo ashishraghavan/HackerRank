6fcb0a1f6f5b0c753263efc118bab211d95c7601
ashishraghavan <ashish13687@icloud.com>
2022-03-23 21:14:40 +0530

    Updated problem ChocolateFeast - correct logic; all passing test cases.

--- src/ChocolateFeast.java
+++ src/ChocolateFeast.java
@@ -1,4 +1,4 @@
-//TODO : Resolve logic issue
+
 public class ChocolateFeast {
     public static void main(String[] args) {
         //15,3,2
@@ -8,25 +8,43 @@ public class ChocolateFeast {
         //10000,260,50
         //10000, 10000, 2
         //50000,480,20
-
-        //Failing
         //7,3,2
-        System.out.println(chocolateFeast(7,3,2));
+        System.out.println(chocolateFeastII(7,3,2));
     }
 
-    //2 <= n <= 100000
-    //1 <= c <= n
-    //2 <= m <= n
-    static int chocolateFeast(int n, int c, int m) {
-        int start = n/c;
-        int wrap = n % c;
-        int cnt = start;
-        while ((start + wrap) >= m) {
-            wrap += start;
-            start = wrap/m;
-            cnt += start;
-            wrap = wrap % m;
+    /*
+    * n = 7, c = 3, m = 2
+    * 7/3 = 2 bars = 2 wrappers = 1 bar = 1 wrapper = end
+    * 2 + 1 = 3 bars
+    * wrap = n/c;
+    * cnt = n/c;
+    * while(wrap >= m) {
+    *
+    * n = 15, c = 3, m = 2
+    * barsConsumed = 15/3 = 5;
+    * wrappers = 5;
+    * barsFromWrapper = 5/2 = 2
+    * wrappers = (5%2) + 2 = 3
+    * barsConsumed = 5 + 2 = 7
+    * 3 >= 2
+    * barsFromWrapper = 3/2 = 1
+    * wrappers = (3%2) + 1 = 2
+    * barsConsumed = 7 + 1 = 8
+    * 2 >= 2
+    * barsFromWrapper = 2/2 = 1
+    * wrappers = (2%2) + 1 = 1
+    * barsConsumed = 8 + 1 = 9
+    * 1 !>= 2 return barsConsumed = 9
+    * */
+    static int chocolateFeastII(int n, int c, int m) {
+        int barsConsumed = n/c;
+        int wrappers = barsConsumed;
+        int barsFromWrapper;
+        while(wrappers >= m) {
+            barsFromWrapper = wrappers/m;
+            wrappers = (wrappers % m) + barsFromWrapper;
+            barsConsumed += barsFromWrapper;
         }
-        return cnt;
+        return barsConsumed;
     }
 }
